eec0d34109f589e92278b133d5381cc9d04c806f
offlinevid <jared.r.vasquez@my.scccd.edu>
2026-01-22 13:58:46 -0800

    1/22/26 implimented constructor, failed to develop my program

--- Pair.h
+++ Pair.h
@@ -1,5 +1,5 @@
 // Pair.h
-// Your Names
+//jared vasquez
 #ifndef PAIR_H
 #define PAIR_H
 
@@ -9,11 +9,49 @@ private:
     T first;
     T second;
 public:
-    Pair(T f, T s);
-    T getFirst() const;
+    Pair(T f, T s);// in reference to pair int; shows the pair that sotores two intergers. creating a version of the parent class 
+    T getFirst() const;// t is the return type
     T getSecond() const;
-    void setFirst(T f);
+    void setFirst(T f);// t is the setter dunction
     void setSecond(T s);
 };
+
+
+
+
+
+int main() {
+
+    Pair<int>p1(3, 7);
+        
+    }
+template <typename T>
+Pair <T>::Pair(T f, T s) {
+
+    First = f;
+    Second = s;
+}
+template <typename T>
+    T Pair<T>::getSecond()const {
+    
+        return 0;
+    }
+    template <typename T>
+    T Pair<T>::getFirst()const {
+
+        return 0;
+    }
+    template <typename T>
+    void Pair<T>::setSecond()const {
+
+        return 0;
+    }
+    template <typename T>
+    void Pair<T>::setFirst()const {
+
+        return 0;
+    }
+
+    }
 
 #endif
